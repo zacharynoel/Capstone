4ab9dcd6ef3c0d618bea63f6445a235df1a812df
Malcolm Craney <mjcraney@calpoly.edu>
2019-03-07 13:51:07 -0800

    Graph

--- app/src/main/java/com/example/varuns/capstone/ReportsActivity.java
+++ app/src/main/java/com/example/varuns/capstone/ReportsActivity.java
@@ -9,6 +9,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Filterable;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.varuns.capstone.model.Artisan;
@@ -16,9 +20,13 @@ import com.example.varuns.capstone.model.SoldItem;
 import com.example.varuns.capstone.services.ApiService;
 import com.example.varuns.capstone.services.RestfulResponse;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import retrofit2.Call;
@@ -32,17 +40,67 @@ public class ReportsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
 
+        // generate Dates
+        Calendar calendar = Calendar.getInstance();
+        Date d1 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d2 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d3 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d4 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d5 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d6 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d7 = calendar.getTime();
+
+
         GraphView graph = (GraphView) findViewById(R.id.graph);
+
+// you can directly pass Date objects to DataPoint-Constructor
+// this will convert the Date to double via Date#getTime()
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(d1, 1),
+                new DataPoint(d2, 5),
+                new DataPoint(d3, 3),
+                new DataPoint(d4, 1),
+                new DataPoint(d5, 5),
+                new DataPoint(d6, 3),
+                new DataPoint(d7, 6)
         });
+
         graph.addSeries(series);
 
+        graph.setTitle("Products Sold");
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3);
+
+        graph.getViewport().setMinX(d1.getTime());
+        graph.getViewport().setMaxX(d7.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.getGridLabelRenderer().setNumVerticalLabels(7);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(6);
+        graph.getViewport().setYAxisBoundsManual(true);
+
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
         setupBottomNavigationView();
+
+        /*Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.values.fake_artisans, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);*/
+
+        //getArtisans();
     }
 
     private void setupBottomNavigationView() {
@@ -73,7 +131,6 @@ public class ReportsActivity extends AppCompatActivity {
         });
     }
 
-
     public void getArtisans() {
         Call<RestfulResponse<List<SoldItem>>> call = ApiService.itemService().getSoldItemsByUserId("1");
         //handle the response
@@ -81,6 +138,10 @@ public class ReportsActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<RestfulResponse<List<SoldItem>>> call, Response<RestfulResponse<List<SoldItem>>> response) {
                 List<SoldItem> soldItems = response.body().getData();
+                System.out.println(soldItems.size());
+                //for (SoldItem i : soldItems) {
+                    //System.out.println("item");
+                //}
                 Toast.makeText(ReportsActivity.this, "success", Toast.LENGTH_SHORT).show();
             }
 
