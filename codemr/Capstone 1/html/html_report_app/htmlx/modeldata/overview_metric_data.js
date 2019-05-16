97c23f543c4f122de7d7ba5ffab95452e830b7bf
Malcolm Craney <mjcraney@calpoly.edu>
2019-05-16 11:16:27 -0700

    removed codemr, save project space

--- codemr/Capstone 1/html/html_report_app/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"Capstone working set: {app}", "Number of Highly Problematic Classes":0, "Number of Entities":1, "Number of Problematic Classes":0, "Class Lines of Code":67, "Number of External Packages":12, "Number of Packages":1, "Number of External Entities":16}
