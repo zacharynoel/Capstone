f66ec0e82f8ad07856e9f1fb33e6297a90d2c557
Zak <zaknoel@gmail.com>
2019-06-03 22:42:35 -0700

    Typo from last commit (password length)

--- app/src/main/java/com/example/varuns/capstone/LoginActivity.java
+++ app/src/main/java/com/example/varuns/capstone/LoginActivity.java
@@ -612,7 +612,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     Toast.makeText(LoginActivity.this, "Registration must include an email", Toast.LENGTH_SHORT).show();
                 else if(mPassword.isEmpty())
                     Toast.makeText(LoginActivity.this, "Registration must include a password", Toast.LENGTH_SHORT).show();
-                else if(mPassword.length() < 4)
+                else if(mPassword.length() < 5)
                     Toast.makeText(LoginActivity.this, "Password must contain 5 or more characters", Toast.LENGTH_SHORT).show();
                 System.out.println("Unsuccessful registration");
             }
